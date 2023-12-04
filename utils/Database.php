68df4413508d3054ff5254308d475f76283cb676
tim <timdesmet00@gmail.com>
2023-12-04 14:45:19 +0100

    connection a la db

--- utils/Database.php
+++ utils/Database.php
@@ -1,35 +1,75 @@
 <?php
 
-declare(strict_types = 1);
+// declare(strict_types = 1);
 
-namespace App\Models;
+// namespace App\Models;
 
-require_once './Helpers/log.php';
+// require_once './Helpers/log.php';
 
-class DatabaseManager {
-    private $pdo;
+// class DatabaseManager {
+//     private $pdo;
 
-    public function __construct() {
-        global $host, $dbname, $charset, $login, $password;
+//     public function __construct() {
+//         global $host, $dbname, $charset, $login, $password;
 
+//         try {
+//             $this->pdo = new \PDO(
+//                 "mysql:host={$host};dbname={$dbname};charset={$charset}",
+//                 $login, 
+//                 $password
+//             );
+//         } catch(Exception $e) {
+//             die('Erreur : '.$e->getMessage());
+//         }
+//     }
+
+//     public function query($sql) {
+//         $stmt = $this->pdo->prepare($sql);
+//         $stmt->execute();
+//         return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+//     }
+
+//     public function getPdo() {
+//         return $this->pdo;
+//     }
+// }
+
+
+declare(strict_types=1);
+
+namespace App\utils;
+
+use PDO;
+use PDOException;
+
+class Database 
+{
+    private Log $log;
+    private ?PDO $pdo = null;
+
+    public function __construct()
+    {
+        $this->log = new Log();
+    }
+
+    public function connectDB()
+    {
         try {
-            $this->pdo = new \PDO(
-                "mysql:host={$host};dbname={$dbname};charset={$charset}",
-                $login, 
-                $password
-            );
-        } catch(Exception $e) {
-            die('Erreur : '.$e->getMessage());
+            $dsn = "{$this->log->host};{$this->log->db}";
+            $this->pdo = new PDO($dsn, $this->log->user, $this->log->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo 'connection réussie';
+        }
+        catch(PDOException $e)
+        {
+            echo 'erreur de connection: ' . $e->getMessage();
         }
     }
 
     public function query($sql) {
-        $stmt = $this->pdo->prepare($sql);
-        $stmt->execute();
-        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-    }
-
-    public function getPdo() {
-        return $this->pdo;
+        if ($this->pdo === null) {
+            throw new \Exception('Must connect to DB before querying');
+        }
+        return $this->pdo->query($sql);
     }
 }
