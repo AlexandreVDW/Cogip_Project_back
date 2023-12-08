fb653915feae15732e248b6d5345aeb52a269e08
tim <timdesmet00@gmail.com>
2023-12-08 09:30:51 +0100

    correction de nom de colonne dans invoices

--- model/Invoices.php
+++ model/Invoices.php
@@ -37,14 +37,14 @@ class Invoices
         return $result;
     }
 
-    public function setNewInvoices($ref, $company_id, $created_at, $updated_at)
+    public function setNewInvoices($ref, $id_company, $created_at, $updated_at)
     {
         $pdo = new Database();
         $conn = $pdo->connectDB();
-        $sql = "INSERT INTO invoices (ref, company_id, created_at, updated_at) VALUES (:ref, :company_id, :created_at, :updated_at)";
+        $sql = "INSERT INTO invoices (ref, id_company, created_at, updated_at) VALUES (:ref, :id_company, :created_at, :updated_at)";
         $stmt = $conn->prepare($sql);
         $stmt->bindValue(':ref', $ref);
-        $stmt->bindValue(':company_id', $company_id);
+        $stmt->bindValue(':id_company', $id_company);
         $stmt->bindValue(':created_at', $created_at);
         $stmt->bindValue(':updated_at', $updated_at);
         $stmt->execute();
